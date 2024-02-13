d90f9e3f6570b1c976373600d536ec684a010992
Michael <mwu250@gmail.com>
2024-02-12 21:13:42 -0800

    feat: created BagInterface and other files still need to add javadocs for the BagInterface

--- BagInterface.java
+++ BagInterface.java
@@ -1,3 +1,31 @@
 public interface BagInterface<T> {
-    
+
+    int getCurrentSize();
+
+    boolean isFull();
+
+    boolean isEmpty();
+
+    boolean add(T newEntry);
+
+    T remove();
+
+    boolean remove(T entry);
+
+    void clear();
+
+    int getFrequencyOf(T entry);
+
+    boolean contains(T entry);
+
+    T[] toArray();
+
+    // added via BagSome project requirements
+    T[] union(T[] inputBag);
+
+    T[] intersection(T[] inputBag);   
+
+    T[] difference(T[] inputBag);
+
+
 }
