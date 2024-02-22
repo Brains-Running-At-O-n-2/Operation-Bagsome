8c1c508c2a84eb2c806beb7a78fd5d990c744021
Hoang Quan Dinh <hoangquandinh0104@gmail.com>
2024-02-21 22:59:37 -0800

    Driver updates/error correction

--- BagDriver.java
+++ BagDriver.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class BagDriver {
     public static void main(String[] args) {
         // Creations of bags containing names of basketball players and football players & List of names of each categories
         BagInterface<String> bBallPlayers = new ResizableArrayBag<>();
         BagInterface<String> fBallPlayers = new LinkedBag<>();
-        String[] bBallPlayerList = {"Erick, Alice, John, John, John, Alex, Alex, Denis, Sadi"};
-        String[] fBallPlayerList = {"Alice, John, Alex, Alex, Alex, Denis, Denis, Tara, Daria, Mori"};
+        String[] bBallPlayerList = {"Erick", "Alice", "John", "John", "John", "Alex", "Alex", "Denis", "Sadi"};
+        String[] fBallPlayerList = {"Alice", "John", "Alex", "Alex", "Alex", "Denis", "Denis", "Tara", "Daria", "Mori"};
         //Adding contents to their respective bags
         for (String sb: bBallPlayerList){
             if (!bBallPlayers.add(sb)){
@@ -16,12 +18,15 @@ public class BagDriver {
                 System.out.println ("Unable to add " + sf + " to LinkedBag");
             } //end if
         } //end for
+        //Verifying contents of bag:
+        System.out.println("Players that plays basketball: " + Arrays.toString(bBallPlayers.toArray()));
+        System.out.println("Players that plays football: " + Arrays.toString(fBallPlayers.toArray()));
         //Check for union between two bags
-        System.out.println ("Players that plays either basketball or football: " + bBallPlayers.union(fBallPlayers).toArray().toString());
+        System.out.println ("Players that plays either basketball or football: " + Arrays.toString(bBallPlayers.union(fBallPlayers).toArray()));
         //Check for intersection between two bags
-        System.out.println ("Players that plays both basketball and football: " + bBallPlayers.intersection(fBallPlayers).toArray().toString());
+        System.out.println ("Players that plays both basketball and football: " + Arrays.toString(bBallPlayers.intersection(fBallPlayers).toArray()));
         //Check for difference between two bags, both ways
-        System.out.println ("Players who plays basketball but not football: " + bBallPlayers.difference(fBallPlayers).toArray().toString());
-        System.out.println ("Players who plays football but not basketball: " + fBallPlayers.difference(bBallPlayers).toArray().toString());
+        System.out.println ("Players who plays basketball but not football: " + Arrays.toString(bBallPlayers.difference(fBallPlayers).toArray()));
+        System.out.println ("Players who plays football but not basketball: " + Arrays.toString(fBallPlayers.difference(bBallPlayers).toArray()));
     } //end main
 }
